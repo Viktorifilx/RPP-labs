56822a9683411842dd04f18d0b8bda5cc533f4e5
Виктория Филатова <filatova.v.m.0606@mail.ru>
2026-09-18 19:42:27 +0300

    Complete Docker lab

---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+from datetime import datetime
+
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
+
+db = SQLAlchemy(app)
+
+
+class Visit(db.Model):
+    __tablename__ = "visits"
+
+    id = db.Column(db.Integer, primary_key=True)
+    visit_time = db.Column(db.DateTime, nullable=False)
+    ip_address = db.Column(db.String(45), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    visit = Visit(
+        visit_time=datetime.now(),
+        ip_address=request.remote_addr
+    )
+
+    db.session.add(visit)
+    db.session.commit()
+
+    return "Hello", 200
